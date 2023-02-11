c92778b5c1b1806f176923f6ae51a8fd7f0d94ec
r00t1024 <r00t1024@gmail.com>
2023-02-12 00:06:29 +0100

    Code cleanup

--- include/flash.h
+++ include/flash.h
@@ -1,11 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <gccore.h>
-#include <wiiuse/wpad.h>
-#include <gctypes.h>
-#include <fat.h>
-
 #ifndef FLASH_H_
 #define FLASH_H_
 
--- src/flash.h
+++
@@ -1,7 +0,0 @@
-#ifndef TOOLS_H_
-#define TOOLS_H_
-
-void dumpBlock(const char *filename, int block);
-void flashBlock(const char *filename, int block);
-
-#endif
