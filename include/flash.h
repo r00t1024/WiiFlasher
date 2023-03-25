3d265cb79f636f1ddf4524c835cdf1fe85020b63
r00t1024 <rootz1024@gmail.com>
2023-03-25 15:40:13 +0100

    Rewritten Library

--- include/flash.h
+++ include/flash.h
@@ -1,13 +1,22 @@
+#include "tools.h"
+
 #ifndef FLASH_H_
 #define FLASH_H_
 
-void eraseBlocks(int firstBlock, int lastBlock);
-void eraseBlock(int block);
+#define MISSING_FILE -1
+#define ERASE_ERROR  -2
+#define SEEK_ERROR   -3
+#define BAD_BLOCK    -13
 
-void dumpBlocks(const char *filename, int firstBlock, int lastBlock);
-void flashBlocks(const char *filename, int firstBlock, int lastBlock);
+struct Simulation{
+	u8* blocksStatus;
+	int toBeWritten;
+};
 
-void dumpBlock(const char *filename, int block);
-void flashBlock(const char *filename, int block);
+s32 NANDFlashInit();
+s32 flashFile(const char* fileName, int firstBlock, int lastBlock, struct Simulation* sim);
+struct Simulation flashFileSim(const char* fileName, int firstBlock, int lastBlock);
+s32 dumpPages(const char* fileName, int firstPage, int lastPage);
+s32 eraseNAND();
 
 #endif
--- src/flash.c
+++ src/flash.c
@@ -4,140 +4,216 @@
 #include "flash.h"
 #include "tools.h"
 
-void eraseBlocks(int firstBlock, int lastBlock){
-	s32 fd = -1;
-	int rv;
+s32 fd = -1;
 
-	fd = IOS_Open("/dev/flash", IPC_OPEN_WRITE);
-	if (fd < 0) {
-		printf("Failed to open /dev/flash (fd = %d)\n", fd);
-		WaitExit();
-	}
-
-	for(int block = firstBlock; block < lastBlock + 1; block++){
-		rv = IOS_Seek(fd, block * 64, 0);
-		if (rv < 0){
-			printf("Failed to seek to block %d (rv = %d)\n", block, rv);
-			WaitExit();
-		}
-		printf("Erasing block %d...\n", block);
-		rv = IOS_Ioctl(fd, 3, NULL, 0, NULL, 0); // Erase block
-		if(rv < 0){
-			printf("Failed to erase block %d (rv = %d)\n", block, rv);
-			WaitExit();
-		}
-	}
-
-	IOS_Close(fd);
+s32 NANDFlashInit(){
+	fd = IOS_Open("/dev/flash", IPC_OPEN_RW);
+	if (fd < 0)
+		return fd;
+	return 0;
 }
 
-void eraseBlock(int block){
-	eraseBlocks(block, block);
+
+s32 __isPageEmpty(u8* data){
+	u8 emptyPage[NAND_PAGE_SIZE];
+	memset(emptyPage, 0xFF, NAND_PAGE_SIZE);
+	
+	return (!memcmp(data, emptyPage, NAND_PAGE_SIZE));
 }
 
-void dumpBlocks(const char *filename, int firstBlock, int lastBlock){
-	static unsigned char buffer[NAND_BLOCK_SIZE] __attribute__ ((aligned(32)));
-	s32 fd = -1;
+s32 __readPage(u8* data, int pageno){
+	static unsigned char buffer[NAND_PAGE_SIZE] __attribute__ ((aligned(32)));
 	int rv;
-
-	printf("Dumping blocks...\n");
-
-	FILE *fout = fopen(filename, "wb");
-	if(fout == NULL){
-		printf("Error: cannot open file %s\n", filename);
-		WaitExit();
-	}
-
-	fd = IOS_Open("/dev/flash", IPC_OPEN_READ);
-	if (fd < 0) {
-		printf("Failed to open /dev/flash (fd = %d)\n", fd);
-		WaitExit();
-	}
-
-	rv = IOS_Seek(fd, firstBlock * 64, 0);
-	if (rv < 0){
-		printf("Failed to seek to block %d (rv = %d)\n", firstBlock, rv);
-		WaitExit();
-	}
-
-	for (int page = firstBlock * 64; page < (lastBlock + 1) * 64; page++) {
-		if(page % 64 == 0)
-			printf("Dumping block %d...\n", page / 64);
-
-		rv = IOS_Seek(fd, page, 0);
-		if (rv < 0){
-			printf("Failed to seek to page %d (rv = %d)\n", page, rv);
-			WaitExit();
-		}
-
-		rv = IOS_Read(fd, buffer, (u32) NAND_PAGE_SIZE);
-
-		fwrite(buffer, NAND_PAGE_SIZE, 1, fout);
-	}
-
-	IOS_Close(fd);
-	fclose(fout);
+	
+	rv = IOS_Seek(fd, pageno, 0);
+	if (rv < 0)
+		return SEEK_ERROR;
+	rv = IOS_Read(fd, buffer, (u32) NAND_PAGE_SIZE);
+	
+	memcpy(data, buffer, NAND_PAGE_SIZE);
+	
+	return 0;
 }
 
-void flashBlocks(const char *filename, int firstBlock, int lastBlock){
-	static unsigned char buffer[NAND_BLOCK_SIZE] __attribute__ ((aligned(32)));
-	s32 fd = -1;
+s32 __flashPage(u8* data, int pageno){
+	static unsigned char buffer[NAND_PAGE_SIZE] __attribute__ ((aligned(32)));
 	int rv;
+	
+	if(__isPageEmpty(data))
+		return 0;
+	
+	memcpy(buffer, data, NAND_PAGE_SIZE);
+	
+	IOS_Seek(fd, pageno, 0);
+	IOS_Write(fd, buffer, (u32) NAND_PAGE_SIZE);
+	
+	return 0;
+}
 
-	printf("Flashing blocks...\n");
+s32 __eraseBlock(int blockno){
+	int rv;
+	
+	rv = IOS_Seek(fd, blockno * 64, 0);
+	if (rv < 0)
+		return SEEK_ERROR;
 
-	FILE *fin = fopen(filename, "rb");
+	rv = IOS_Ioctl(fd, 4, NULL, 0, NULL, 0); // Is this block bad?
+	if(rv == BAD_BLOCK)
+		return BAD_BLOCK;
+	
+	rv = IOS_Ioctl(fd, 3, NULL, 0, NULL, 0); // Erase block
+	if(rv < 0)
+		return ERASE_ERROR;
+	
+	return 0;
+}
+
+s32 __flashBlock(u8* data, int blockno){
+	int rv;
+	u8 nandPage[NAND_PAGE_SIZE];
+	
+	rv = __eraseBlock(blockno);
+	if(rv < 0)
+		return rv;
+	
+	for(int pg = 0; pg < 64; pg++){
+		memcpy(nandPage, data+pg*NAND_PAGE_SIZE, NAND_PAGE_SIZE);
+		__flashPage(nandPage, pg+blockno*64);
+	}
+	
+	return 0;
+}
+
+struct Simulation __simulateWrite(const char* fileName, int firstBlock, int lastBlock){
+	int rv;
+	struct Simulation sim;
+	u8* nandPage = (u8*)malloc(NAND_PAGE_SIZE);
+	u8* filePage = (u8*)malloc(NAND_PAGE_SIZE);
+	//u8 nandPage[NAND_PAGE_SIZE];
+	//u8 filePage[NAND_PAGE_SIZE];
+	
+	sim.blocksStatus = (u8*)malloc(lastBlock - firstBlock + 1);
+	sim.toBeWritten  = 0;
+	
+	// 0: No need to write
+	// 1: Needs to be erased and written
+	// 2: Bad block
+	
+	FILE *fin = fopen(fileName, "rb");
 	if(fin == NULL){
-		printf("Error: cannot open file %s\n", filename);
-		WaitExit();
+		printf("Cannot open file!\n");
+		free(sim.blocksStatus);
+		sim.blocksStatus = NULL;
+		return sim;
 	}
-
-	fd = IOS_Open("/dev/flash", IPC_OPEN_WRITE);
-	if (fd < 0) {
-		printf("Failed to open /dev/flash (fd = %d)\n", fd);
-		WaitExit();
-	}
-
-	for(int block = firstBlock; block < lastBlock + 1; block++){
-		rv = IOS_Seek(fd, block * 64, 0);
-		if (rv < 0){
-			printf("Failed to seek to block %d (rv = %d)\n", block, rv);
-			WaitExit();
+	
+	for(int block = firstBlock; block <= lastBlock; block++){
+		printf(" [+] Analyzing block %d... ", block);
+		
+		sim.blocksStatus[block - firstBlock] = 0;
+		
+		IOS_Seek(fd, block * 64, 0);
+		rv = IOS_Ioctl(fd, 4, NULL, 0, NULL, 0); // Is this block bad?
+		
+		if(rv == BAD_BLOCK){
+			printf("bad\n");
+			sim.blocksStatus[block - firstBlock] = 2;
+			continue;
 		}
-		printf("Erasing block %d...\n", block);
-		rv = IOS_Ioctl(fd, 3, NULL, 0, NULL, 0); // Erase block
-		if(rv < 0){
-			printf("Failed to erase block %d (rv = %d)\n", block, rv);
-			WaitExit();
+		
+		for(int pg = block*64; pg < (block+1)*64; pg++){
+			rv = __readPage(nandPage, pg);
+			fseek(fin, pg*NAND_PAGE_SIZE, SEEK_SET);
+			fread(filePage, NAND_PAGE_SIZE, 1, fin);
+			
+			if(memcmp(nandPage, filePage, NAND_PAGE_SIZE)){
+				sim.blocksStatus[block - firstBlock] = 1;
+				
+				if(!__isPageEmpty(filePage))
+					sim.toBeWritten++;
+			}
 		}
+		
+		if(sim.blocksStatus[block - firstBlock] == 1)
+			printf("modified\n");
+		else
+			printf("intact\n");
 	}
-
-	for (int page = firstBlock * 64; page < (lastBlock + 1) * 64; page++) {
-		if(page % 64 == 0)
-			printf("Flashing block %d...\n", page / 64);
-
-		rv = IOS_Seek(fd, page, 0);
-		if (rv < 0){
-			printf("Failed to seek to page %d (rv = %d)\n", page, rv);
-			WaitExit();
-		}
-
-		fread(buffer, NAND_PAGE_SIZE, 1, fin);
-
-		rv = IOS_Write(fd, buffer, (u32) NAND_PAGE_SIZE);
-
-		if(rv != 2112)
-			printf("Write error: page %d block %d (rv = %d)\n", page, page / 64, rv);
-	}
-
-	IOS_Close(fd);
-	fclose(fin);
+	
+	free(nandPage);
+	free(filePage);
+	
+	return sim;
 }
 
-void dumpBlock(const char *filename, int block){
-	dumpBlocks(filename, block, block);
+s32 flashFile(const char* fileName, int firstBlock, int lastBlock, struct Simulation* sim){
+	int rv;
+	u8* data = (u8*)malloc(NAND_BLOCK_SIZE);
+	
+	FILE *fin = fopen(fileName, "rb");
+	if(fin == NULL)
+		return MISSING_FILE;
+	
+	for(int block = firstBlock; block <= lastBlock; block++){
+		if(sim != NULL){
+			if(sim->blocksStatus[block - firstBlock] == 1){
+				printf(" [+] Flashing block %d...\n", block);
+				fread(data, NAND_BLOCK_SIZE, 1, fin);
+				__flashBlock(data, block);
+			}
+		}
+		else{
+			printf(" [+] Flashing block %d...\n", block);
+			fread(data, NAND_BLOCK_SIZE, 1, fin);
+			__flashBlock(data, block);
+		}
+	}
+	
+	return 0;
 }
 
-void flashBlock(const char *filename, int block){
-	flashBlocks(filename, block, block);
+struct Simulation flashFileSim(const char* fileName, int firstBlock, int lastBlock){
+	int blocksToModify = 0;
+	struct Simulation sim = __simulateWrite(fileName, firstBlock, lastBlock);
+	
+	printf("Bad blocks:\n");
+	for(int block = firstBlock; block <= lastBlock; block++){
+		if(sim.blocksStatus[block-firstBlock] == 2)
+			printf(" [-] block %d\n", block);
+		else if(sim.blocksStatus[block-firstBlock] == 1)
+			blocksToModify++;
+	}
+	
+	printf("Will erase %d pages (%d blocks)...\n", blocksToModify*64, blocksToModify);
+	printf("Will write %d pages...\n", sim.toBeWritten);
+	
+	return sim;
+}
+
+s32 dumpPages(const char* fileName, int firstPage, int lastPage){
+	u8* nandPage = (u8*)malloc(NAND_PAGE_SIZE);
+	
+	FILE *fout = fopen(fileName, "wb");
+	if(fout == NULL){
+		printf("Cannot open file!\n");
+		return MISSING_FILE;
+	}
+	
+	for(int page = firstPage; page <= lastPage; page++){
+		__readPage(nandPage, page);
+		fwrite(nandPage, NAND_PAGE_SIZE, 1, fout);
+	}
+	
+	fclose(fout);
+	free(nandPage);
+	
+	return 0;
+}
+
+s32 eraseNAND(){
+	for(int block = 0; block < 4096; block++){
+		printf(" [+] Erasing block %d...\n", block);
+		__eraseBlock(block);
+	}
+	return 0;
 }
